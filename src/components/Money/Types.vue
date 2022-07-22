5879b7e1ddc8c9ab28aaa7f1918be2b981f3b7ec
xiyou <494949543@qq.com>
2022-07-22 23:54:00 +0800

    基本完成 NumberPad 组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -12,41 +12,20 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
 
-@Component
-export default class Types extends Vue {
-  type = '-';  // '-'表示支出,'+'表示收入
-  selectType(type: string) {
-    if (type !== '-' && type !== '+') {
-      throw new Error('type is unknown');
+  @Component
+  export default class Types extends Vue {
+    type = '-';  // '-'表示支出,'+'表示收入
+    selectType(type: string) {
+      if (type !== '-' && type !== '+') {
+        throw new Error('type is unknown');
     }
     this.type = type;
   }
 }
 
-//
-// export default {
-// name: "Types",
-//   props:['xxx'],
-//   data(){
-//   return {
-//     type:'-'// '-'表示支出,'+'表示收入
-//   }
-//   },
-//   mounted(){
-//   console.log(this.xxx)
-//   },
-//   methods:{
-//     selectType(type){ //type只能是 '-' 和 '+' 中的一个
-//       if(type !== '-' && type !== '+') {
-//         throw new Error('type is unknown')
-//       }
-//       this.type = type
-//     }
-//   }
-// }
 </script>
 
 <style lang="scss" scoped>
