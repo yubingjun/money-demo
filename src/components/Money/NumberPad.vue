8462f28c599ecef930d7f7a6052e939ebe290c07
xiyou <494949543@qq.com>
2022-07-22 22:43:52 +0800

    完成 Types 组件

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -5,11 +5,11 @@
       <button>1</button>
       <button>2</button>
       <button>3</button>
-      <button>sc</button>
+      <button>删除</button>
       <button>4</button>
       <button>5</button>
       <button>6</button>
-      <button>qk</button>
+      <button>清空</button>
       <button>7</button>
       <button>8</button>
       <button>9</button>
--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -2,34 +2,51 @@
   <div>
     <ul class="types">
       <li :class="type === '-' && 'selected'"
-      @click="selectType('-')">支出</li>
+          @click="selectType('-')">支出
+      </li>
       <li :class="type === '+' && 'selected'"
-      @click="selectType('+')">收入</li>
+          @click="selectType('+')">收入
+      </li>
     </ul>
   </div>
 </template>
 
-<script lang="js">
-export default {
-name: "Types",
-  props:['xxx'],
-  data(){
-  return {
-    type:'-'// '-'表示支出,'+'表示收入
-  }
-  },
-  mounted(){
-  console.log(this.xxx)
-  },
-  methods:{
-    selectType(type){ //type只能是 '-' 和 '+' 中的一个
-      if(type !== '-' && type !== '+') {
-        throw new Error('type is unknown')
-      }
-      this.type = type
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Types extends Vue {
+  type = '-';  // '-'表示支出,'+'表示收入
+  selectType(type: string) {
+    if (type !== '-' && type !== '+') {
+      throw new Error('type is unknown');
     }
+    this.type = type;
   }
 }
+
+//
+// export default {
+// name: "Types",
+//   props:['xxx'],
+//   data(){
+//   return {
+//     type:'-'// '-'表示支出,'+'表示收入
+//   }
+//   },
+//   mounted(){
+//   console.log(this.xxx)
+//   },
+//   methods:{
+//     selectType(type){ //type只能是 '-' 和 '+' 中的一个
+//       if(type !== '-' && type !== '+') {
+//         throw new Error('type is unknown')
+//       }
+//       this.type = type
+//     }
+//   }
+// }
 </script>
 
 <style lang="scss" scoped>
