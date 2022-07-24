001c9de0fe95f87f42eedb4f3046a4d5004d35b5
xiyou <494949543@qq.com>
2022-07-24 12:43:54 +0800

    把 record 的复制保存到list

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -21,42 +21,45 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
 
-  @Component
-  export default class NumberPad extends Vue {
-    output = '0';
+@Component
+export default class NumberPad extends Vue {
+  // output = '0';
+  @Prop() readonly value!: number;
+  output = this.value.toString();
 
-    inputContent(event:MouseEvent) {
-      const button = (event.target as HTMLButtonElement);
-      const input = button.textContent;
-      if(this.output.length === 16) {return;}
-      if(this.output === '0') {
-        if('0123456789'.indexOf(input) >= 0) {
-          this.output = input;
-        } else {
-          this.output += input;
-        }
-        return;
-      }
-      if(this.output.indexOf('.') >= 0 && input === '.') {return;}
-      this.output += input
-    }
-
-    remove() {
-      if(this.output.length === 1) {
-        this.output = '0';
+  inputContent(event:MouseEvent) {
+    const button = (event.target as HTMLButtonElement);
+    const input = button.textContent!;
+    if(this.output.length === 16) {return;}
+    if(this.output === '0') {
+      if('0123456789'.indexOf(input) >= 0) {
+        this.output = input;
       } else {
-        this.output = this.output.slice(0,-1);
+        this.output += input;
       }
+      return;
     }
-    clear() {
+    if(this.output.indexOf('.') >= 0 && input === '.') {return;}
+    this.output += input
+  }
+
+  remove() {
+    if(this.output.length === 1) {
       this.output = '0';
+    } else {
+      this.output = this.output.slice(0,-1);
     }
-    ok() {
-      this.$emit('update:value',this.output);
-    }
+  }
+  clear() {
+    this.output = '0';
+  }
+  ok() {
+    this.$emit('update:value',this.output);
+    this.$emit('submit',this.output)
+  }
 }
 </script>
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
-    {{record}}
-    <NumberPad :value="record.amount" @update:value="onUpdateAmount"/>
+    {{ record }}
+    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
@@ -14,7 +14,7 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Watch} from 'vue-property-decorator';
 
 type Record = {
   tags: string[]
@@ -27,16 +27,30 @@ type Record = {
   components: {Tags, Notes, Types, NumberPad},
 })
 export default class Money extends Vue {
-      tags = ['衣', '食' , '住' , '行'];
-      record: Record = {
-        tags:[], notes:'', type:'-', amount:0
-      }
-      onUpdateTags(value: string[]) {
-        this.record.tags = value
-      }
-      onUpdateNotes(value: string) {
-        this.record.notes = value
-      }
+  tags = ['衣', '食', '住', '行'];
+  recordList: Record[] = [];
+  record: Record = {
+    tags: [], notes: '', type: '-', amount: 0
+  }
+
+  onUpdateTags(value: string[]) {
+    this.record.tags = value
+  }
+
+  onUpdateNotes(value: string) {
+    this.record.notes = value
+  }
+
+  saveRecord() {
+    const record2 = JSON.parse(JSON.stringify(this.record))
+    this.recordList.push(record2)
+    console.log(this.recordList);
+  }
+
+  @Watch('recordList')
+  onRecordListChange() {
+    window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+  }
 }
 </script>
 
