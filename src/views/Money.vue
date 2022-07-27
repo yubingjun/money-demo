82fda9cab9b4eee7811ea9bd0b2bd226086588ee
xiyou <494949543@qq.com>
2022-07-27 22:40:23 +0800

    封装 tagList 的增删改查

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -20,11 +20,8 @@ import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component, Watch} from 'vue-property-decorator';
 import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
 
 const recordList = recordListModel.fetch()
-
-
 
 
 @Component({
