cbc152b918d8f43cea5d61cb9e88c815b97be9d5
xiyou <494949543@qq.com>
2022-07-27 17:01:36 +0800

    封装 window.createTag

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -32,12 +32,7 @@ export default class Labels extends Vue {
   createTag() {
     const name = window.prompt('请输入标签名');
     if (name) {
-      const message = tagListModel.create(name);
-      if (message === 'duplicated') {
-        window.alert('标签名重复了');
-      } else if(message === 'success') {
-        window.alert('添加成功')
-      }
+     window.createTag(name)
     }
   }
 }
