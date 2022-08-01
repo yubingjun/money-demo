7e221eed6740c20e25995173103bd6557834f904
xiyou <494949543@qq.com>
2022-08-01 23:16:49 +0800

    修复了一些小问题

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -5,9 +5,10 @@
     <div class="notes">
       <FormItem field-name="备注"
                 placeholder="在这里输入备注"
-                @update:value="onUpdateNotes"/>
+                :value.sync="record.notes"
+      />
     </div>
-    <Tags/>
+    <Tags @update:value="record.tags = $event"/>
   </Layout>
 </template>
 
@@ -43,7 +44,14 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
+    if(!this.record.tags || this.record.tags.length === 0){
+      return window.alert('请至少选择一个标签')
+    }
     this.$store.commit('createRecord', this.record);
+    if(this.$store.state.createRecordError === null) {
+      window.alert('记账成功')
+      this.record.notes = ''
+    }
   }
 }
 </script>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-    <ol>
+    <ol v-if="groupedList.length > 0">
       <li v-for="(group, index) in groupedList" :key="index">
         <h3 class="title">{{ beautify(group.title) }} <span>{{group.total}}￥</span></h3>
         <ol>
@@ -14,6 +14,9 @@
         </ol>
       </li>
     </ol>
+    <div v-else class="noResult">
+      目前没有相关记录
+    </div>
   </Layout>
 </template>
 
@@ -31,7 +34,7 @@ import clone from '@/lib/clone';
 })
 export default class Statistics extends Vue {
   tagString(tags: Tag[]) {
-    return tags.length ===0 ? '无' : tags.join(',')
+    return tags.length === 0 ? '无' : tags.map(t => t.name).join('，')
   }
   beautify(string: string) {
     const day = dayjs(string)
@@ -54,11 +57,11 @@ export default class Statistics extends Vue {
 
   get groupedList() {
     const {recordList} = this;
-    if(recordList.length === 0) {return []}
 
     const newList = clone(recordList)
         .filter(r=>r.type===this.type)
         .sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf())
+    if(newList.length === 0) {return []}
     type Result = { title: string, total?: number, items: RecordItem[] }[]
     const result: Result = [ { title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'),items: [newList[0]]}]
     for(let i = 1; i < newList.length; i++) {
@@ -89,6 +92,10 @@ export default class Statistics extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.noResult {
+  padding: 16px;
+  text-align: center;
+}
 ::v-deep {
   .type-tabs-item {
     background: #c4c4c4;
